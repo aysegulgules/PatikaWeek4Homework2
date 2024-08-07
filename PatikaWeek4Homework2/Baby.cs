9521a25d7ab32d821ac0d4c001d6bb46b23c55b5
aysegulgules <159700271+aysegulgules@users.noreply.github.com>
2024-08-07 10:39:24 +0300

    Add project files.

---
+++ PatikaWeek4Homework2/Baby.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaWeek4Homework2
+{
+    public class Baby
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        //Default Constructor
+        public Baby()
+        {
+            BirthDate = DateTime.Now;            
+            Console.WriteLine("Ingaaaa ");
+        }
+
+        //ikinci Constructor
+        public Baby(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;    
+            BirthDate=DateTime.Now;
+            Console.WriteLine("\nBebeğin Adı-Soyadı :" + FirstName + " " + LastName);
+            Console.WriteLine("Bebeğin doğum tarihi..:" + BirthDate.ToShortDateString());
+            Console.WriteLine("Ingaaaa ");
+        }
+
+
+        /// <summary>
+        /// Default constructor new'leme anında çalıştığı için FirstName ve LastName property'leri null dır.
+        /// Bundan dolayı bu metod default constructor ile newlenen nesneyi ekrana yazdırmak için oluşturuldu.
+        /// </summary>
+        public void BabyInfo()
+        {
+            Console.WriteLine("Bebeğin Adı-Soyadı :" + FirstName+" "+ LastName);
+            Console.WriteLine("Bebeğin doğum tarihi..:" + BirthDate.ToShortDateString());
+        }
+    }
+}
---
+++ PatikaWeek4Homework2/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using PatikaWeek4Homework2;
+
+
+//Default Constructor ile nesne oluşturuluyor
+Baby baby = new Baby();
+baby.FirstName = "Tarık";
+baby.LastName = "Aslan";
+baby.BabyInfo();
+
+//ikinci Constructor ile nesne oluşturuluyor
+Baby baby1 = new Baby("Beyza", "Kaya");
+
